e6899c5cc594bf24ba54d967a76066cbf7f66544
Aleksander Dyatlov <aleksander.dyaloff@gmail.com>
2022-11-02 16:11:02 +0200

    f

--- components/TextField/component.tsx
+++ components/TextField/component.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import { Props } from "./types";
 import * as styles from './styles.css'
+import classNames from "classnames";
 
 const Component: FC<Props> = ({
+  className,
   ...restProps
 }) => {
   return (
-    <label className={styles.wrapper}>
+    <label className={classNames(styles.wrapper, className)}>
       <input className={styles.input} {...restProps} />
     </label>
   )
--- pages/styles.css.ts
+++ pages/styles.css.ts
@@ -7,14 +7,49 @@ export const app = style({
   backgroundColor: color('backgroundMain'),
 })
 
-export const avatar = style({
-  borderRadius: '50%',
+export const user = style({
+  userSelect: 'none',
+  cursor: 'pointer',
+  display: 'flex',
+  fontSize: 14,
+  alignItems: 'center',
+  padding: '4px 8px',
+  transitionProperty: 'background-color',
+  transitionDuration: '120ms',
+  borderRadius: 10,
+
+  ':hover': {
+    backgroundColor: color('backgroundMain'),
+  }
 })
 
-export const user = style({
-  display: 'flex',
-  alignItems: 'center',
-  padding: 4,
+export const userCheckbox = style({
+  display: 'none',
+})
+
+export const searchField = style({
+  marginBottom: 8,
+})
+
+export const icon = style({
+  width: '1rem',
+  height: '1rem',
+  marginLeft: 'auto',
+  color: 'gray',
+})
+
+export const userDisabled = style({
+  color: 'gray',
+})
+
+export const avatar = style({
+  borderRadius: '50%',
+  marginRight: 8,
+  selectors: {
+    [`${userDisabled} &`]: {
+      filter: 'grayscale(100%)',
+    },
+  }
 })
 
 export const tasksFilter = style({
